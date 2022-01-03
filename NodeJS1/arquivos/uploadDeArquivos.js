32efa22528e179c86ea352000d5af310857b4093
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-03 11:14:31 -0300

    filtrando se o arquivo tem extensao de imagem para salvar

--- NodeJS1/arquivos/uploadDeArquivos.js
+++ NodeJS1/arquivos/uploadDeArquivos.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
 
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+    if (tipoEhValido === -1) {
+        console.log("erro: tipo invalido")
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
