a86426b981452e67c453170f0a73d295a24992a4
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-03 10:10:16 -0300

    Criando tabela PETS
    corrigindo erro e atualizando arquivos do projeto

--- NodeJS1/arquivos/uploadDeArquivos.js
+++ NodeJS1/arquivos/uploadDeArquivos.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-fs.createReadStream('C:\\Users\\Administrador\\Desktop\\Codigos\\Codigos estagio\\NodeJS\\NodeJS1\\assets\\salsicha.jpg')
-    .pipe(fs.createWriteStream('C:\\Users\\Administrador\\Desktop\\Codigos\\Codigos estagio\\NodeJS\\NodeJS1\\assets\\salsicha-stream.jpg'))
+fs.createReadStream('./assets/salsicha.jpg')
+    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
     .on('finish', () => console.log("imagem escrita"))
--- NodeJS1/infraestrutura/tabelas.js
+++ NodeJS1/infraestrutura/tabelas.js
@@ -18,7 +18,7 @@ class Tabelas {
     }
 
     criarPets(){
-        const query = 'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))'
+        const query = 'CREATE TABLE IF NOT EXISTS Pets(id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))'
 
         this.conexao.query(query, erro => {
             if(erro) {
