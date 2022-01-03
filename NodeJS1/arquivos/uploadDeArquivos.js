d555edfa039fb3a3d3e16e91824eb7c71516e2c4
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-03 11:08:15 -0300

    Ler arquivo com nome variavel e salvar ele

--- NodeJS1/arquivos/uploadDeArquivos.js
+++ NodeJS1/arquivos/uploadDeArquivos.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 
-fs.createReadStream('./assets/salsicha.jpg')
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log("imagem escrita"))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+
+    fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(novoCaminho))
+}
