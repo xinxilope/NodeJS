dd05045cfc8b835ed87631282f8c2438fa86d42e
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-06 11:46:45 -0300

    Update index.js

--- NodeORM/api/index.js
+++ NodeORM/api/index.js
@@ -3,14 +3,13 @@ const bodyParser = require('body-parser')
 
 const app = express() //inicializo servidor
 
-app.use(bodyParser.json()) //converter informacoes para json
+app.use(bodyParser.json()) //converter informacoes para json quando passar por qualquer rota (middleware)
 
 const port = 3000 //porta do servidor
 
-app.get('/teste', (req, res) => res //definir a rota: http://localhost:3000/teste
-    .status(200)
-    .send({ mensagem: 'boas vindas à API' //servidor responde com um objeto com essa mensagem dentro
-}))
+app.get('/teste', (req, res) => //definir a rota: http://localhost:3000/teste
+    res.status(200).send({ mensagem: 'boas vindas à API' }) //servidor responde com um objeto com essa mensagem dentro
+)
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`)) //servidor rodando com essa mensagem
 
