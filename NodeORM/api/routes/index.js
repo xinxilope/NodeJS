3912216e3cc741858c25fbca24691924c3d05dac
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-07 16:18:43 -0300

    criando controller/router para niveis e turmas

--- NodeORM/api/routes/index.js
+++ NodeORM/api/routes/index.js
@@ -1,9 +1,13 @@
 const bodyParser = require('body-parser')
 const pessoas = require('./pessoasRoute')
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
 
 //define as rotas da aplicacao
 module.exports = app => {
     app.use(bodyParser.json())
     
     app.use(pessoas)
+    app.use(niveis)
+    app.use(turmas)
 }
