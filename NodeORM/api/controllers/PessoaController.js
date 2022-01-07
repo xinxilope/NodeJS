33b50e1b3c362c67b9fcb7092ceebb609c37d965
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-07 08:48:39 -0300

    criando controller para Pessoa
    e criando funcao PegaTodasAsPessoas

---
+++ NodeORM/api/controllers/PessoaController.js
@@ -0,0 +1,14 @@
+const database = require('../models/index')
+
+class PessoaController {
+    static async pegaTodasAsPessoas(req, res) {
+        try {
+            const todasAsPessoas = await database.Pessoas.findAll() //async para achar todas as pessoas na database
+            return res.status(200).json(todasAsPessoas) //retorna todasAsPessoas em JSON com status 200
+        } catch (error) {
+            return res.status(500).json(error.message) //tenta retornar as pessoas, e caso tenha erro devolve o erro
+        }
+    }
+}
+
+module.exports = PessoaController
