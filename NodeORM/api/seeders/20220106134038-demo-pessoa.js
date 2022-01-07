2327e1576a670aa987b9dbe29af471ecf385d0f3
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-07 15:55:00 -0300

    usando os seeds para popular tabelas

--- NodeORM/api/seeders/20220106134038-demo-pessoa.js
+++ NodeORM/api/seeders/20220106134038-demo-pessoa.js
@@ -64,6 +64,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('Pessoas', null, {});
   }
 };
