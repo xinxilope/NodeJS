22182be0e1e1772b9fc75e279b09a70d48ca0861
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-11 19:11:56 -0300

    fix: removendo require desnecessario

--- NodeDESAFIO/api/routes/notasRoute.js
+++ NodeDESAFIO/api/routes/notasRoute.js
@@ -1,5 +1,4 @@
 const {Router} = require('express')
-const { bemvindo } = require('../controllers/NotasController')
 const Notas = require('../controllers/NotasController')
 
 const router = Router()
