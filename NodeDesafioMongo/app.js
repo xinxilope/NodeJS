d5541e2c7828bbba643f7929b601de6ee5d9df0b
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-13 08:47:30 -0300

    criando rotas, controllers, e o app main

--- NodeDesafioMongo/app.js
+++ NodeDesafioMongo/app.js
@@ -1,1 +1,28 @@
-console.log('teste!')
+const express = require('express')
+const tasks = require('./routes/tasks')
+
+const app = express()
+
+
+
+//middleware
+app.use(express.json())
+
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send("Task manager app")
+})
+
+
+
+
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+const port = 3000
+
+app.listen(3000, console.log(`server is listening on port ${port}...`))
---
+++ NodeDesafioMongo/routes/tasks.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require('../controllers/tasks')
+
+
+router.route('/').get(getAllTasks)
+router.route('/').post(createTask)
+router.route('/:id').get(getTask)
+router.route('/:id').patch(updateTask)
+router.route('/:id').delete(deleteTask)
+
+
+
+module.exports = router
