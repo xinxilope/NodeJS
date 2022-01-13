56ad98ecc2f6d992675f3632f827c72f6b026b0a
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-13 15:22:51 -0300

    criando GET para uma task

--- NodeDesafioMongo/controllers/tasks.js
+++ NodeDesafioMongo/controllers/tasks.js
@@ -20,8 +20,17 @@ const criaTask = async (req, res) => {
     }
 }
 
-const listaUmaTask = (req, res) => {
-    res.json({id:req.params.id})
+const listaUmaTask = async (req, res) => {
+    try {
+        const {id:taskId} = req.params
+        const umaTask = await Task.findOne({_id:taskId})
+        if (!umaTask) {
+            return res.status(404).json({msg:`Nao existe task com o id: ${taskId}`})
+        }
+        res.status(200).json(umaTask)
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 const updateTask = (req, res) => {
